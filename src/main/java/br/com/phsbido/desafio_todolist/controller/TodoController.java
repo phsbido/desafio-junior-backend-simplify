8340cf85ddde2054dfcf3ac3e9e1d7d01d919413
phsbido <pedrobido.ph@gmail.com>
2024-09-05 09:44:08 -0300

    configuração do controller

---
+++ src/main/java/br/com/phsbido/desafio_todolist/controller/TodoController.java
@@ -0,0 +1,39 @@
+package br.com.phsbido.desafio_todolist.controller;
+
+import br.com.phsbido.desafio_todolist.entity.Todo;
+import br.com.phsbido.desafio_todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    public List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    public List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    public List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+
+}
